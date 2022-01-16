bdaf92e215840a8a6fdc1a296ec93e5e64d36520
Shiyue Zhang <zhangs@carleton.edu>
2022-01-16 10:01:12 -0600

    fixes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name='listFile',
+    name='listAllTheFile',
     packages = find_packages(),
     version='0.0.1',
     license='MIT',
@@ -10,7 +10,7 @@ setup(
      url="https://github.com/zhangshyue/listFile",
     entry_points={
         'console_scripts': [
-            'list_file=listFile.list_file:extract'
+            'list_all_the_file=listFile.list_file:extract'
         ]
     }
 )
